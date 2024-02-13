1170ec1ce48b33a6b5c910828c6c9708c4b2e695
ElsaStromback <elsastrmbck@gmail.com>
2024-02-13 13:59:57 +0100

    Added a script called subprocess.py

---
+++ database/subprocess.py
@@ -0,0 +1,23 @@
+import subprocess
+import sys
+import os
+
+# Define the command as a list of strings
+command = [
+    'python3',
+    '/home/administrator/cbsa_tools/view.py',  # Full path to view.py
+    '--dataset', 'WORLDCUP98',
+    '--part', 'wc_day90_1',
+    '--input', '/home/administrator/cbsa_tools/worldcup98.zip',  # Full path to input file
+    '--start', '1998-07-24T00:48:00',
+    '--duration', '1h',
+    '--format', 'json'
+]
+working_directory = '/home/administrator/masterthesis/database'
+
+
+# Run the command using subprocess
+try:
+    subprocess.run(command, check=True, cwd=working_directory)
+except subprocess.CalledProcessError as e:
+    print(f"An error occurred: {e}")
