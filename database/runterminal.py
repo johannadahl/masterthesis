9d01242573aebd795587584e30406f1a70e53020
ElsaStromback <elsastrmbck@gmail.com>
2024-02-13 15:01:52 +0100

    Small changes

--- database/runterminal.py
+++ database/runterminal.py
@@ -5,10 +5,10 @@ import os
 # Define the command as a list of strings
 command = [
     'python3',
-    '/home/administrator/cbsa_tools/view.py',  # Full path to view.py
+    '/app/cbsa_tools/view.py',  # Full path to view.py
     '--dataset', 'WORLDCUP98',
     '--part', 'wc_day90_1',
-    '--input', '/home/administrator/worldcup98.zip',  # Full path to input file
+    '--input', '/app/cbsa_tools/worldcup98.zip',  # Full path to input file
     '--start', '1998-07-24T00:48:00',
     '--duration', '1h',
     '--format', 'json'
