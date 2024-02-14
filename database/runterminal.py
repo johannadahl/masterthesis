66ce4fc9808bd48395d12bd043407d4420e41017
ElsaStromback <elsastrmbck@gmail.com>
2024-02-14 13:52:50 +0100

    small changes runterminal

--- database/runterminal.py
+++ database/runterminal.py
@@ -15,40 +15,18 @@ def print_result(cursor):
         print(row)
 
 def main():
-    
+
     data_list = []
     for line in sys.stdin:
         data = json.loads(line)
             # Append the parsed JSON data to the list
         data_list.append(data)
     df = pd.DataFrame(data_list)
+    df_counts = df.groupby('time').size().reset_index(name='requests')
     print(df)
-    try:
-        connection = mysql.connector.connect(user='root', 
-                                            password='root',
-                                            host='127.0.0.1',
-                                            port = '3306',
-                                            database = 'Elsa_Containers'
-                                            )
-        print("Connected to MySQL database successfully")
-
-        cursor = connection.cursor()
-        send_query(cursor, "select * from worldcup_requests;",0)
-        # Fetch all rows from the result set
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-        cursor.close()
+    print(df_counts)
 
 
-    except mysql.connector.Error as error:
-        print("Error while connecting to MySQL:", error)
-
-    finally:
-        if 'connection' in locals():
-            cursor.close()
-            connection.close()
-            print("MySQL connection closed")
 
 if __name__ == '__main__':
     main()
