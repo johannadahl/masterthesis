634500405b0fac2376b5866cdc7846b29ada6e4b
ElsaStromback <elsastrmbck@gmail.com>
2024-02-14 13:44:41 +0100

    small changes runterminal

--- database/runterminal.py
+++ database/runterminal.py
@@ -1,13 +1,54 @@
 import sys
 import json
 import pandas as pd
+import mysql.connector 
+
+def send_query(cursor, query, values):
+    if values != 0:
+        cursor.execute(query,values)
+    else: 
+        cursor.execute(query)
+
+def print_result(cursor):
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+def main():
+    
+    data_list = []
+    for line in sys.stdin:
+        data = json.loads(line)
+            # Append the parsed JSON data to the list
+        data_list.append(data)
+    df = pd.DataFrame(data_list)
+    print(df)
+    try:
+        connection = mysql.connector.connect(user='root', 
+                                            password='root',
+                                            host='127.0.0.1',
+                                            port = '3306',
+                                            database = 'Elsa_Containers'
+                                            )
+        print("Connected to MySQL database successfully")
+
+        cursor = connection.cursor()
+        send_query(cursor, "select * from worldcup_requests;",0)
+        # Fetch all rows from the result set
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+        cursor.close()
 
 
+    except mysql.connector.Error as error:
+        print("Error while connecting to MySQL:", error)
 
-data_list = []
-for line in sys.stdin:
-    data = json.loads(line)
-        # Append the parsed JSON data to the list
-    data_list.append(data)
-df = pd.DataFrame(data_list)
-print(df)
+    finally:
+        if 'connection' in locals():
+            cursor.close()
+            connection.close()
+            print("MySQL connection closed")
+
+if __name__ == '__main__':
+    main()
