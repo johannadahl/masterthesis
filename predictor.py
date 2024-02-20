fc7c8769c29d875a790960397632936e433b17d6
ElsaStromback <elsastrmbck@gmail.com>
2024-02-20 15:26:37 +0100

    Changes in predictor.py. Simulates may month of workload

--- predictor.py
+++ predictor.py
@@ -1,8 +1,15 @@
 ##Börja testa ML här 
 from database.queryTool import fetch_and_return_data
+import matplotlib.pyplot as plt
+import pandas as pd
 
+def plot_workload(df):
+  #  df = df.set_index('time')
+    plt.plot(df.index, df['method_count'])
+    plt.show()
 
 if __name__ == "__main__":
-    data_result = fetch_and_return_data("1998-05", "60s")
+    df = fetch_and_return_data("1998-05", "60s")
+    plot_workload(df)
 
     print("hej")
