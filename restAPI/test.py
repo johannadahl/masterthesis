0ded6aa4a55d6b1ebe0671846f8ccd95b26dd348
ElsaStromback <elsastrmbck@gmail.com>
2024-02-21 10:20:50 +0100

    Flaskapp server for loadgenerator. And made it possible to do get requests

--- restAPI/test.py
+++ restAPI/test.py
@@ -2,8 +2,8 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE+"GetNames/Johanna") #Skickar en GET request
-print(response.json())
+#response = requests.post(BASE+"Workload/45") #Skickar en post request
+#print(response.json())
 
-response = requests.post(BASE+"Workload/45") #Skickar en post request
+response = requests.get(BASE+"generate_load/1998-05/10s") #Skickar en GET request
 print(response.json())
