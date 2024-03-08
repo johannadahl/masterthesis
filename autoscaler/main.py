c9ec37c2d82c1307abe426311855a0a91d6f63f6
ElsaStromback <elsastrmbck@gmail.com>
2024-03-08 15:03:21 +0100

    small changes

--- autoscaler/main.py
+++ autoscaler/main.py
@@ -1,10 +1,14 @@
 ##Skelettkod för en predictive autoscaler 
+#Allt är oklart haha
 
 class Scaler():
+    # Räknar ut hur mycket instanser som behövs baserat på uträknade värdet? 
     print("Scale")
 
 class Predictor():
+    #Använder sig av en Prediktionsmodell på gammal historisk data 
     print("Predict")
 
 if __name__ == "__main__":
+    #Ta emot en request en gång per dag om att target service vill veta prediktade värdet för upcoming load?
     print("!")
