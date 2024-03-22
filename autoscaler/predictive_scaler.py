cac874473c4b946819f95797c8262fbb19fca205
ElsaStromback <elsastrmbck@gmail.com>
2024-03-22 11:13:42 +0100

    Added a Prophet predictor class and added to the predictive_scaler script

--- autoscaler/predictive_scaler.py
+++ autoscaler/predictive_scaler.py
@@ -2,24 +2,33 @@
 from flask import Flask, request
 import numpy as np
 from XGBoost_predictor import XGBoostPredictor
+from Prophet_predictor import ProphetPredictor
 import threading
 
 app = Flask(__name__)
 xgboost_predictor = XGBoostPredictor()
+prophet_predictor = ProphetPredictor()
 
 
 @app.route("/predict", methods=["POST"])
 def predict():
+    """""
+    Här kommer man när target service startar. Den skickar en request om vad predikterade värderna är för loaden som kommer börja köras på systemet. 
+    """""
     start_date = request.json.get('start_date', None)
     end_date = request.json.get('end_date', None)
+    """""
+    Ändrade det här, så nu är det bara en rad som man måste "ändra" om man vill byta prediktionsmodell (kommentera ut de som inte ska användas)
+    Detta kan struktureras upp bättre sen med args men funkar så länge,
+    """""
     df_predictions = generate_predictions_with_xgboost(start_date, end_date)
+  # df_predictions = generate_predictions_with_prophet(start_date, end_date)
     payload = df_predictions.reset_index().to_json(orient='records')
     return payload
 
     
 def create_and_train_xgboost_predictor():
     df = xgboost_predictor.import_historical_dataset()
-    print(df)
     if df is not None:
         df = xgboost_predictor.preprocess_data(df)
         df = xgboost_predictor.remove_outliers(df)
@@ -28,7 +37,7 @@ def create_and_train_xgboost_predictor():
         prediction_model, predictions, scores = xgboost_predictor.preform_cross_validation(df)
         print(f'Score across folds {np.mean(scores):0.4f}')
         print(f'Fold scores:{scores}')
-        xgboost_predictor.visualize_CV_predictions(xgboost_predictor.model,df)
+    #    xgboost_predictor.visualize_CV_predictions(xgboost_predictor.model,df)
     return xgboost_predictor
 
 def generate_predictions_with_xgboost(start_date, end_date):
@@ -41,12 +50,35 @@ def generate_predictions_with_xgboost(start_date, end_date):
     return df_with_predictions
 
    
+def create_and_train_prophet_predictor():
+    df = prophet_predictor.import_historical_dataset()
+    if df is not None:
+        df = prophet_predictor.preprocess_data(df)
+        df = prophet_predictor.remove_outliers(df)
+        df = prophet_predictor.create_features(df)
+        df = prophet_predictor.add_lags_prophet(df)
+        df = prophet_predictor.fix_prophet_format(df)
+        model = prophet_predictor.create_model(df)
+    return model
+
+def generate_predictions_with_prophet(start_date, end_date):
+    df = prophet_predictor.generate_X_values(start_date, end_date)
+    df['applied_load'] = None
+    df = prophet_predictor.create_features(df)
+    df = prophet_predictor.add_lags_prophet(df) 
+    df_with_predictions = prophet_predictor.make_predictions(df)
+    df_with_predictions.drop(columns=df_with_predictions.columns.difference(['ds','yhat']), inplace=True)
+    df_with_predictions.rename(columns={"yhat": "pred","ds":"index" }, inplace=True)
+    return df_with_predictions
+
 def start_flask():
     app.run(debug=False, port=8010,use_reloader=False, host='0.0.0.0') #Startar flask server för TargetService på en annan tråd! 
 
 if __name__ == "__main__":
+    """""
+    Här tränas först modellerna innan servern startas! Sen kan severn bara igång om man vill testa lite olika target service värden.
+    """"" 
     create_and_train_xgboost_predictor()
-    
+    create_and_train_prophet_predictor()
     flask_thread = threading.Thread(target=start_flask) #Flaskservern måste köras på en egen tråd! annars kan man inte köra annan kod samtidigt 
     flask_thread.start()
-   # predict_future_with_xgboost(xgboost_predictor)
